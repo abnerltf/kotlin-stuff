3cb0a15843767dd90389687d800d055ec9a9b9d6
Abner Lima <abner.lima@agenciarazzo.com.br>
2021-06-19 09:41:24 -0300

    Adiciona app de sorteio de números

---
+++ SorteioNumero/app/src/main/java/com/example/sorteionumero/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.sorteionumero
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import android.view.View;
+import android.widget.TextView;
+import java.util.Random;
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun SortearNumero(view: View){
+        var texto = findViewById(R.id.txtNumero) as TextView
+
+        var numeroSorteado = Random().nextInt(11)
+
+        texto.setText("O número é: $numeroSorteado")
+    }
+}
