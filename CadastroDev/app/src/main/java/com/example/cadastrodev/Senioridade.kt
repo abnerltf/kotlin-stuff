e0354fa5ddc822de855ae3dad5c1b39d122e1729
Abner Lima <abnerltf@gmail.com>
2021-07-08 20:06:35 -0300

    Adicionar aplicativo de cadastro de devs

---
+++ CadastroDev/app/src/main/java/com/example/cadastrodev/Senioridade.kt
@@ -0,0 +1,7 @@
+package com.example.cadastrodev
+
+enum class Senioridade(val nome: String) {
+    JUNIOR("Junior"),
+    PLENO("Pleno"),
+    SENIOR("Senior")
+}
---
+++ CadastroDev/app/src/main/java/com/example/cadastrodev/Stack.kt
@@ -0,0 +1,7 @@
+package com.example.cadastrodev
+
+enum class Stack(val nome: String) {
+    FRONTEND("Frontend"),
+    BACKEND("Backend"),
+    FULLSTACK("Fullstack")
+}
